a0401d7f92f3c2a7311cfa312e63367a90dc3be7
DanielOhn <ohndaniel@gmail.com>
2022-10-19 16:12:15 -0700

    Update Player script to include animations.

--- objects/Player.gd
+++ objects/Player.gd
@@ -15,6 +15,10 @@ onready var primary_attackrate : Timer = $PrimaryAttackrate
 onready var secondary_attackrate : Timer = $SecondaryAttackrate
 onready var primary_attack : MeshInstance = $Front/PrimaryHitbox/CollisionShape/MeshInstance
 
+onready var animations : AnimationPlayer = $PlayerModel/Wizard/AnimationPlayer
+onready var animTree : AnimationTree = $AnimationTree
+onready var animState : AnimationNodeStateMachinePlayback = animTree.get("parameters/playback")
+
 func _ready():
 	rig.set_as_toplevel(true)
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
@@ -23,10 +27,7 @@ func _physics_process(delta):
 	camera_follow()
 	player_movement(delta)
 	player_rotation()
-	
 	abilities()
-	if (primary_attackrate.get_time_left() > 0):
-		print(primary_attackrate.get_time_left())
 	
 func camera_follow():
 	var player_pos = global_transform.origin
@@ -39,9 +40,12 @@ func player_movement(delta):
 	input_vector.z = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	
 	if input_vector != Vector3.ZERO:
+		animState.travel("Run")
+		
 		input_vector = input_vector.normalized()
 		velocity = input_vector * speed
 	else:
+		animState.travel("Idle")
 		velocity = velocity.move_toward(Vector3.ZERO, friction * delta)
 	
 	return move_and_slide(velocity)
@@ -65,32 +69,33 @@ func player_rotation():
 		look_at(look, Vector3.UP)
 	
 func abilities():
+	# Left Click Attack - Sword attack that hits in a small cone in front of player
 	if Input.is_action_just_pressed("primary_ability") and primary_attackrate.get_time_left() == 0:
-		primary_ability.disabled = false
-		primary_attack.visible = true
-		
-		primary_attackrate.start()
-		
-	elif primary_attackrate.get_time_left() <= 0.35 or primary_attackrate.get_time_left() == 0:
-		primary_ability.disabled = true
-		primary_attack.visible = false
+		#primary_ability.disabled = false
+		#primary_attack.visible = true
 	
+		# Starts timer
+		primary_attackrate.start()
+		animState.travel("Attack")
+		
+	elif primary_attackrate.get_time_left() <= 0.4:
+		#primary_ability.disabled = true
+		#primary_attack.visible = false
+		pass
+	
+	# Right Click Attack - Shoots a ranged projectile to target location
 	if Input.is_action_pressed("secondary_ability") and secondary_attackrate.get_time_left() == 0:
-		var new_projectile = Projectile.instance()
-		new_projectile.global_transform = $Front.global_transform
-		new_projectile.speed = 40
-		var scene_root = get_tree().get_root().get_children()[0]
-		scene_root.add_child(new_projectile)
+		animState.travel("Casting")
 		
-		secondary_attackrate.start()
 		
-		#var mouse_pos = get_viewport().get_mouse_position()
-		#var ray_origin = camera.project_ray_origin(mouse_pos)
-		#var ray_target = camera.project_ray_normal(mouse_pos) * 1000
-
-		#var space_state = get_world().direct_space_state
-		#var intersection = space_state.intersect_ray(ray_origin, ray_target)
-
+func shoot():
+	var new_projectile = Projectile.instance()
+	new_projectile.global_transform = $Front.global_transform
+	new_projectile.speed = 40
+	var scene_root = get_tree().get_root().get_children()[0]
+	scene_root.add_child(new_projectile)
+	
+	secondary_attackrate.start()
 
 func _on_PrimaryHitbox_body_entered(body):
 	print(body)
