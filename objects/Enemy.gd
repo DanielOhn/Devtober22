45048a86fa934965a69ef4be4f6cdfa46010c64c
unknown <ohndaniel@gmail.com>
2022-10-26 23:04:56 -0700

    Fix issue with knockback.  Updated States to work properly with animations.

--- objects/Enemy.gd
+++ objects/Enemy.gd
@@ -27,62 +27,56 @@ func changeState(new_state):
 func _physics_process(delta):
 	# Check State of Enemy
 	if state == "Hurt":
-		pass
+		if wallstun > 0:
+			hitstun = 0
+			wallstun -= 1
+		
+		elif hitstun > 0:
+			animState.travel("walk")
+			var direction = path[path_index] - global_transform.origin
+			var knockback : Vector3 = player.global_transform.origin - global_transform.origin
+			hitstun -= 1
+			
+			direction.y = 0
+			move_and_collide(-knockback.normalized() * 40 * delta)
+		else:
+			hitstun = 0
+			changeState(states[0])
 
 	if state == "Attack":
 		animState.travel("attack")
 	
 	if state == "Movement":
 		if path_index < path.size():
-			if wallstun != 0:
-				hitstun = 0
-				wallstun -= 1
-				# PLAY STUN ANIMATION
-			else:
-				var direction = path[path_index] - global_transform.origin
-				var distance = global_transform.origin.distance_to(player.global_transform.origin)
-				#if direction == Vector3(0, 0, 0):
-				#	state = states[1]
-				print(global_transform.origin.distance_to(player.global_transform.origin))
-				
-				if distance < 1.5:
-					state = states[1]
-				
-				if hitstun != 0:
-					animState.travel("walk")
-					var knockback : Vector3 = player.global_transform.origin - global_transform.origin
-					hitstun -= 1
-					
-					direction.y = 0
-					move_and_collide(-knockback.normalized() * 40 * delta)
-				
-				else:
-					knockback = 0
-					if direction.length() < 1:
-						path_index += 1
-					else:
-						var pos = player.global_transform.origin
-						pos.y = 90
-						
-						look_at(pos, Vector3.UP)
-						animState.travel("run")
-						move_and_slide(direction.normalized() * speed, Vector3.UP)
+			var direction = path[path_index] - global_transform.origin
+			var distance = global_transform.origin.distance_to(player.global_transform.origin)
 			
-				 
-
+			if distance < 1.5:
+				state = states[1]
+				
+			else:
+				if direction.length() < 1:
+					path_index += 1
+				else:
+					var pos = player.global_transform.origin
+					pos.y = 90
+					
+					look_at(pos, Vector3.UP)
+					animState.travel("run")
+					move_and_slide(direction.normalized() * speed, Vector3.UP)
+			
 func hurt(damage, knockback):
 	# Apply damage to health
 	# Check if enemy dies
-	knockback = knockback
+	changeState(states[2])
 	hitstun = 8
 	health = health - damage
+	
 	if (health <= 0):
 		# Play death animation then release
 		# ADD DEATH ANIMATION
 		queue_free()
-	
-	
-	
+		
 func follow():
 	path = nav.get_simple_path(global_transform.origin, player.global_transform.origin)
 	path_index = 0
